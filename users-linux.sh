63d6162eac7931b340da2dfcb3a9d26edacb710a
Terrence Houlahan <terrence.houlahan@open-ipcamera.net>
2019-04-26 08:51:35 +0100

    All files in this commit only had their versions bumped to include them in the tagged release.

--- users-linux.sh
+++ users-linux.sh
@@ -8,7 +8,7 @@ source "${BASH_SOURCE%/*}/functions.sh"
 # Developer:  Terrence Houlahan Linux Engineer F1Linux.com
 # https://www.linkedin.com/in/terrencehoulahan/
 # Contact: terrence.houlahan@open-ipcamera.net
-# Version 01.86.01
+# Version 01.86.02
 
 ######  License: ######
 # Copyright (C) 2018 2019 Terrence Houlahan
